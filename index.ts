2d61274df18a6fd2ca13321fd29aa14655bfee58
parkjongjoon7128 <whdwnsdk8111@naver.com>
2025-04-07 22:30:57 +0900

    feat: kakao 라이브러리 환경설정 진행중

--- index.ts
+++ index.ts
@@ -1,8 +1,12 @@
-import { registerRootComponent } from 'expo';
+import { registerRootComponent } from "expo";
 
-import App from './App';
+import { initializeKakaoSDK } from "@react-native-kakao/core";
+import App from "./App";
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
+
 registerRootComponent(App);
+
+initializeKakaoSDK("c55cbbae2ff680cb54be87022180a1c8");
