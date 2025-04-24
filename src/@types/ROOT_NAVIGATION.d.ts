15cbbad78954ba2ed7c6836eda6c3352d017bc54
parkjongjoon7128 <whdwnsdk8111@naver.com>
2025-04-25 02:55:56 +0900

    feat: react-navigation 설치

---
+++ src/@types/ROOT_NAVIGATION.d.ts
@@ -0,0 +1,3 @@
+declare type ROOT_NAVIGATION = {
+  Login: undefined;
+};
---
+++ src/screens/LoginScreen.tsx
@@ -0,0 +1,14 @@
+import { Text, View } from "react-native";
+
+const LoginScreen = () => {
+  // Logic
+
+  // View
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text>LoginScreen</Text>
+    </View>
+  );
+};
+
+export default LoginScreen;
